2be2d524cec49fd6f22dbf3b8a3346548e1f2f5a
Sean Craig <seancraig21@gmail.com>
2021-10-04 23:04:15 -0500

    Changed the structure of the html and js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,68 +1,107 @@
-var mainEl = document.querySelector(".main");
-var welcomeDiv = document.querySelector(".welcome");
-var quizDiv = document.querySelector(".quiz");
+var $mainEl = document.querySelector(".main");
 
-var startBtn = document.querySelector("#startBtn");
-var timerEl = document.querySelector("#timer");
+var $startScreenDiv = document.getElementById("start-screen");
+var $questionsDiv = document.getElementById("questions");
+var $endScreenDiv = document.getElementById("end-screen");
+var $timerEl = document.getElementById("time");
+var $questionTitle = document.getElementById("question-title");
+var $choicesDiv = document.getElementById("choices");
+// var $choicesList = document.getElementById("choicesList");
 
+var startBtn = document.getElementById("startBtn");
+var submitBtn = document.getElementById("submitBtn");
+
+var questionsArr = [
+  {
+    title: "Commonly used data types DO NOT include: ",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "Arrays is JavaScript can be used to store _______",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+  },
+  {
+    title: "String values must be enclosed within ______ when being assigned to variables",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "parentheses"
+  }
+
+];
+var currentQuestionIndex = 0;
 var secondsLeft = 10;
 
-function loadPage() {
-  timerEl.textContent = secondsLeft;
-  quizDiv.style.display = "none";
+function startQuiz() {
+  // console.log("call startQuiz");
+  $startScreenDiv.className = "hide";
+  $questionsDiv.className = "questions";
+  
+  getQuestion();
+}
+
+function getQuestion() {
+  // Get questions and stuff
+  $questionTitle.textContent = questionsArr[currentQuestionIndex].title;
+  $choicesDiv.innerHTML = "";
+  for (var i = 0; i < 4; i++){
+    var choiceBtn = document.createElement("button");
+    choiceBtn.textContent = questionsArr[currentQuestionIndex].choices[i];
+    $choicesDiv.appendChild(choiceBtn);
+    $choicesDiv.appendChild(document.createElement("br"));
+    choiceBtn.addEventListener("click", questionClick);
+  }
+}
+
+function questionClick(event) {
+  // Capture the input 
+  // Validate it
+  var choice = event.target;
+
+  if (choice.textContent === questionsArr[currentQuestionIndex].answer){
+    console.log("correct");
+  }
+  else {
+    console.log("wrong");
+  }
+
+  currentQuestionIndex++;
+
+  // if (currentQuestionIndex > questionsArr.length){
+  //   gameOver();
+  // } else {
+    getQuestion();
+  // }
+}
+
+function gameOver(){
+  $questionsDiv.className = "hide";
+  $endScreenDiv.className = "end-screen";
+}
+
+function clockTick() {
+
+}
+
+function submitHighScore() {
+
 }
 
 startBtn.addEventListener("click", function () {
   // Start timer
   var timeInterval = setInterval(function () {
     secondsLeft--;
-    timerEl.textContent = secondsLeft;
+    $timerEl.textContent = secondsLeft;
     if (secondsLeft === 0) {
       clearInterval(timeInterval);
-      //gameOver();
+      // gameOver();
     }
   }, 1000);
 
-  // Hide the welcome div
-  welcomeDiv.style.display = "none";
-
-  // Show the first question
+  
   startQuiz();
 });
 
-function startQuiz() {
-  quizDiv.style.display = "flex";
-  // var answerA = document.forms["question"]["choiceA"];
-  // var answerB = document.forms["question"]["choiceB"];
+submitBtn.onclick = submitHighScore;
 
-  // var submitAnswerBtn = document.getElementById("submitBtn");
-  // submitAnswerBtn.addEventListener("click", function(){
-  //   if (answerA){
-  //     console.log("yay!");
-  //   } else {
-  //     console.log("booo");
-  //   }
-  // });
-}
-
-function gameOver() {
-  var gameOverDiv = document.createElement("div");
-  var gameOverTitle = document.createElement("h1");
-  var gameOverText = document.createElement("h2");
-  var submitScoreBtn = document.createElement("button");
-
-  gameOverTitle.textContent = "All done!";
-  gameOverText.textContent = "Your final score is: ";
-  submitScoreBtn.textContent = "Submit";
-
-  mainEl.appendChild(gameOverDiv);
-  gameOverDiv.appendChild(gameOverTitle);
-  gameOverDiv.appendChild(gameOverText);
-  gameOverDiv.appendChild(submitBtn);
-
-  submitScoreBtn.addEventListener("click", function(){
-    window.location.href = "highscores.html";
-  })
-}
-
-loadPage();
+// startBtn.onclick = startQuiz;
