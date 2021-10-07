005ea4217ffd0f690ae6f9a056491f69fa7e0354
Sean Craig <seancraig21@gmail.com>
2021-10-06 19:46:53 -0500

    Added more questions, styled the pages, added gif

--- README.md
+++ README.md
@@ -2,9 +2,19 @@
 
 This is a quiz for testing your knowledge of JavaScript
 
-![This page includes a button to start the quiz, a link to the highscore page and a timer.](./assets/demo/screenshot.png)
+![This page includes a button to start the quiz, a link to the highscore page and a timer.](./assets/demo/code-quiz.gif)
 
 Here is [link](https://seanscraig.github.io/code-quiz/) to the live webpage
+
+# Instructions
+
+1) Click the "Start Quiz" button to begin the quiz
+
+2) Answer the questions by clicking the button with your answer
+
+3) At the end type your initials in to the text box
+
+4) You can clear the highscores by clicking the clear scores button
 
 ## Installation
 
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -14,25 +14,88 @@ var submitBtn = document.getElementById("submitBtn");
 
 var currentQuestionIndex = 0;
 var score = 0;
-var secondsLeft = 40;
+var secondsLeft = 75;
 
 var timeInterval;
 
 var questionsArr = [
   {
-    title: "Commonly used data types DO NOT include: ",
+    title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts"
   },
   {
     title: "Arrays is JavaScript can be used to store _______",
-    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
     answer: "all of the above"
   },
   {
-    title: "String values must be enclosed within ______ when being assigned to variables",
+    title:
+      "String values must be enclosed within ______ when being assigned to variables",
     choices: ["commas", "curly brackets", "quotes", "parentheses"],
     answer: "quotes"
+  },
+  {
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<js>", "<scripting>", "<javascript>", "<script>"],
+    answer: "<script>"
+  },
+  {
+    title:
+      "What is the correct JavaScript syntax to change the content of the HTML element <p id='demo'>This is a demonstration.</p>",
+    choices: [
+      "document.getElementByName('p').innerHTML = 'Hello World!'",
+      "document.getElement('p').innerHTML = 'Hello World!'",
+      "document.getElementByName('demo').innerHTML = 'Hello World!'",
+      "#demo.innerHTML = 'Hello World!'"
+    ],
+    answer: "document.getElementByName('demo').innerHTML = 'Hello World!'"
+  },
+  {
+    title: "Where is the correct place to insert JavaScript?",
+    choices: [
+      "The <head> section",
+      "The <body> section",
+      "All of the above",
+      "None of the above"
+    ],
+    answer: "All of the above"
+  },
+  {
+    title:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choices: [
+      "<script src ='xxx.js'>",
+      "<script name ='xxx.js'>",
+      "<script href ='xxx.js'>",
+      "None of the above"
+    ],
+    answer: "<script src ='xxx.js'>"
+  },
+  {
+    title: "How do you write 'Hello World' in an alert box?",
+    choices: [
+      "alert('Hello World')",
+      "alertBox('Hello World')",
+      "msg('Hello World')",
+      "msgBox('Hello World')"
+    ],
+    answer: "alert('Hello World')"
+  },
+  {
+    title: "How do you create a function in JavaScript?",
+    choices: ["function:myFunction()", "function myFunction()", "function = myFunction()", "None of the above"],
+    answer: "function myFunction()"
+  },
+  {
+    title: "How do you call a function named 'myFunction'?",
+    choices: ["call myFunction()", "call function myFunction()", "myFunction()", "None of the above"],
+    answer: "myFunction()"
   }
 ];
 
@@ -46,7 +109,7 @@ function startQuiz() {
   // Start the timer
   $timerEl.textContent = secondsLeft;
   timeInterval = setInterval(clockTick, 1000);
-  
+
   // Get the first question
   getQuestion();
 }
@@ -55,8 +118,9 @@ function getQuestion() {
   // Get questions and stuff
   $questionTitle.textContent = questionsArr[currentQuestionIndex].title;
   $choicesDiv.innerHTML = "";
-  for (var i = 0; i < 4; i++){
+  for (var i = 0; i < 4; i++) {
     var choiceBtn = document.createElement("button");
+    choiceBtn.classList.add("btn");
     choiceBtn.textContent = questionsArr[currentQuestionIndex].choices[i];
     $choicesDiv.appendChild(choiceBtn);
     $choicesDiv.appendChild(document.createElement("br"));
@@ -65,18 +129,17 @@ function getQuestion() {
 }
 
 function questionClick(event) {
-  // Capture the input 
+  // Capture the input
   // Validate it
   var choice = event.target;
   $resultsDiv.innerHTML = "";
-  if (choice.textContent === questionsArr[currentQuestionIndex].answer){
+  if (choice.textContent === questionsArr[currentQuestionIndex].answer) {
     var $correctEl = document.createElement("h2");
     $correctEl.textContent = "Correct!";
     $correctEl.style.color = "green";
     $resultsDiv.appendChild($correctEl);
-  }
-  else {
-    secondsLeft -= 10;
+  } else {
+    secondsLeft -= 5;
     var $wrongEl = document.createElement("h2");
     $wrongEl.textContent = "Wrong!";
     $wrongEl.style.color = "red";
@@ -85,14 +148,14 @@ function questionClick(event) {
 
   currentQuestionIndex++;
 
-  if (currentQuestionIndex > questionsArr.length - 1){
+  if (currentQuestionIndex > questionsArr.length - 1) {
     gameOver();
   } else {
     getQuestion();
   }
 }
 
-function gameOver(){
+function gameOver() {
   $questionsDiv.className = "hide";
   $endScreenDiv.className = "end-screen";
   score = secondsLeft;
@@ -103,7 +166,7 @@ function gameOver(){
 function clockTick() {
   secondsLeft--;
   $timerEl.textContent = secondsLeft;
-  if (secondsLeft <= 0){
+  if (secondsLeft <= 0) {
     gameOver();
   }
 }
@@ -111,8 +174,8 @@ function clockTick() {
 function submitHighScore() {
   var user = {
     initials: $initialsEl.value,
-    score: score
-  }
+    score: score,
+  };
   if (initials === "") {
     alert("Please enter your initials");
     return;
